0a0d7d2b3d025e7d27e6e1b3397729dd9ef1226b
Stameskaaa <chta02@mail.ru>
2024-11-22 20:56:18 +0500

    change styles and number algorithm

--- src/components/NumberTransition/NumberTransition.tsx
+++ src/components/NumberTransition/NumberTransition.tsx
@@ -8,14 +8,16 @@ const NumberTransition = ({ number }: { number: number }) => {
     if (currentNumber !== number) {
       const interval = setInterval(() => {
         setCurrentNumber((prev) => {
-          if (prev + 100 < number) return prev + 100;
-          if (prev - 100 > number) return prev - 100;
-          if (prev + 10 < number) return prev + 10;
-          if (prev - 10 > number) return prev - 10;
-          if (prev + 5 < number) return prev + 5;
-          if (prev - 5 > number) return prev - 5;
-          if (prev < number) return prev + 1;
-          if (prev > number) return prev - 1;
+          const difference = number - prev;
+          const steps = [100, 10, 5, 1];
+          const step = steps.find((s) => Math.abs(difference) >= s) || 1;
+
+          if (difference > 0) {
+            return prev + step > number ? number : prev + step;
+          } else if (difference < 0) {
+            return prev - step < number ? number : prev - step;
+          }
+
           clearInterval(interval);
           return prev;
         });
